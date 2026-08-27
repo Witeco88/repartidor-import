9e9a918e8101b1d3deab4048de0fd82ed008e8c1
Witeco88 <theplace2find@gmail.com>
2026-08-27 14:07:46 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,1 @@
-const CACHE = "import-pwa-v1";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-180.png", "./icon-512.png"];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const CACHE="repartidor-v2";const A=["./","./index.html","./manifest.json","./sw.js","./icon-180.png","./icon-512.png"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)));self.skipWaiting()});self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
